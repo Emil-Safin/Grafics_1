f589c25ff5c31dee790f0b8dff52672a6caf99c2
Эмиль Сафин <safinemil32@gmail.com>
2022-03-26 19:21:05 +0200

    3

--- Grafics_1/Source.cpp
+++ Grafics_1/Source.cpp
@@ -17,7 +17,7 @@ void Display(void)
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-    glDrawArrays(GL_POINTS, 0, 1);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
     glDisableVertexAttribArray(0);
     glutSwapBuffers();
 
@@ -42,10 +42,12 @@ int main(int argc, char* argv[]) {
         fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
         return 1;
     }
-    vec2 Vertices[1];
-    Vertices[0] = vec2(0.0f, 0.0f);
-    glGenBuffers(1, &VBO);
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    vec3 Vertices[3];
+    Vertices[0] = vec3(1.0f, 1.0f, 0.0f);
+    Vertices[1] = vec3(-1.0f, 0.0f, 0.0f);
+    Vertices[2] = vec3(1.0f, -1.0f, 0.0f);
+    glGenBuffers(1, &VBO); //функций для генерации
+    glBindBuffer(GL_ARRAY_BUFFER, VBO); 
     glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
   
     glutMainLoop();
