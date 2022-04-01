6991501a08307c820f73d82084336e37a625266b
Эмиль Сафин <safinemil32@gmail.com>
2022-04-01 18:05:22 +0200

    8 шаг (без шейдеров)

--- Grafics_1/Source.cpp
+++ Grafics_1/Source.cpp
@@ -12,29 +12,6 @@
 using namespace glm;
 
 GLuint VBO;
-GLuint gWorldLocation;
-
-static const char* pVS = "                                                          \n\
-#version 330                                                                        \n\
-                                                                                    \n\
-layout (location = 0) in vec3 Position;                                             \n\
-                                                                                    \n\
-uniform mat4 gWorld;                                                                \n\
-                                                                                    \n\
-void main()                                                                         \n\
-{                                                                                   \n\
-    gl_Position = gWorld * vec4(Position, 1.0);                                     \n\
-}";
-
-static const char* pFS = "                                                          \n\
-#version 330                                                                        \n\
-                                                                                    \n\
-out vec4 FragColor;                                                                 \n\
-                                                                                    \n\
-void main()                                                                         \n\
-{                                                                                   \n\
-    FragColor = vec4(1.0, 0.0, 0.0, 1.0);                                           \n\
-}";
 
 static void RenderSceneCB()
 {
@@ -43,16 +20,22 @@ static void RenderSceneCB()
     Scale += 0.001f;
 
     glm::mat4x4 World;
+    vec3 Vertices[3];
+    Vertices[0] = vec3(-1.0f, -1.0f, 0.0f);
+    Vertices[1] = vec3(1.0f, -1.0f, 0.0f);
+    Vertices[2] = vec3(0.0f, 1.0f, 0.0f);
 
     World[0][0] = sinf(Scale); World[0][1] = 0.0f;        World[0][2] = 0.0f;        World[0][3] = 0.0f;
     World[1][0] = 0.0f;        World[1][1] = cosf(Scale); World[1][2] = 0.0f;        World[1][3] = 0.0f;
     World[2][0] = 0.0f;        World[2][1] = 0.0f;        World[2][2] = sinf(Scale); World[2][3] = 0.0f;
     World[3][0] = 0.0f;        World[3][1] = 0.0f;        World[3][2] = 0.0f;        World[3][3] = 1.0f;
-
-    glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, &World[0][0]);
-
-    glEnableVertexAttribArray(0);
+    Vertices[0] = World * vec4(Vertices[0], 1.0f);
+    Vertices[1] = World * vec4(Vertices[1], 1.0f);
+    Vertices[2] = World * vec4(Vertices[2], 1.0f);
+    glGenBuffers(1, &VBO); //функций для генерации
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
     glDrawArrays(GL_TRIANGLES, 0, 3);
     glDisableVertexAttribArray(0);
@@ -64,69 +47,6 @@ static void RenderSceneCB()
 
 
 
-static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
-{
-    GLuint ShaderObj = glCreateShader(ShaderType);
-
-    if (ShaderObj == 0) {
-        fprintf(stderr, "Error creating shader type %d\n", ShaderType);
-        exit(0);
-    }
-
-    const GLchar* p[1];
-    p[0] = pShaderText;
-    GLint Lengths[1];
-    Lengths[0] = strlen(pShaderText);
-    glShaderSource(ShaderObj, 1, p, Lengths);
-    glCompileShader(ShaderObj);
-    GLint success;
-    glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
-    if (!success) {
-        GLchar InfoLog[1024];
-        glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
-        fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
-        exit(1);
-    }
-
-    glAttachShader(ShaderProgram, ShaderObj);
-}
-
-static void CompileShaders()
-{
-    GLuint ShaderProgram = glCreateProgram();
-
-    if (ShaderProgram == 0) {
-        fprintf(stderr, "Error creating shader program\n");
-        exit(1);
-    }
-
-    AddShader(ShaderProgram, pVS, GL_VERTEX_SHADER);
-    AddShader(ShaderProgram, pFS, GL_FRAGMENT_SHADER);
-
-    GLint Success = 0;
-    GLchar ErrorLog[1024] = { 0 };
-
-    glLinkProgram(ShaderProgram);
-    glGetProgramiv(ShaderProgram, GL_LINK_STATUS, &Success);
-    if (Success == 0) {
-        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
-        fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
-        exit(1);
-    }
-
-    glValidateProgram(ShaderProgram);
-    glGetProgramiv(ShaderProgram, GL_VALIDATE_STATUS, &Success);
-    if (!Success) {
-        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
-        fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
-        exit(1);
-    }
-
-    glUseProgram(ShaderProgram);
-
-    gWorldLocation = glGetUniformLocation(ShaderProgram, "gWorld");
-    assert(gWorldLocation != 0xFFFFFFFF);
-}
 
 
 
@@ -139,33 +59,13 @@ int main(int argc, char* argv[]) {
     glutDisplayFunc(RenderSceneCB);
     glutIdleFunc(RenderSceneCB);
     
-    //glutDisplayFunc();
-
     GLenum res = glewInit();
     if (res != GLEW_OK)
     {
         fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
         return 1;
     }
-    vec3 Vertices[3];
-    Vertices[0] = vec3(-1.0f, -1.0f, 0.0f);
-    Vertices[1] = vec3(1.0f, -1.0f, 0.0f);
-    Vertices[2] = vec3(0.0f, 1.0f, 0.0f);
-    glGenBuffers(1, &VBO); //функций для генерации
-    glBindBuffer(GL_ARRAY_BUFFER, VBO); 
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
-    CompileShaders();
     glutMainLoop();
-    /////////
-
-   /* GLuint ShaderProgram = glCreateProgram();
-    GLuint ShaderObj = glCreateShader(GL_VERTEX_SHADER);
-
-    const GLchar* p[1];
-    p[0] = pShaderText;
-    GLint Lengths[1];
-    Lengths[0] = strlen(pShaderText);
-    glShaderoSurce(ShaderObj, 1, p, Lengths);*/
 
     return 0;
 }
