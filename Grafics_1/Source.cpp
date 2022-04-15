736bc716ef6030188b2bca1e5d47c78791a6db6b
Эмиль Сафин <safinemil32@gmail.com>
2022-04-15 16:38:54 +0200

    j

--- Grafics_1/Source.cpp
+++ Grafics_1/Source.cpp
@@ -135,9 +135,7 @@ static void RenderSceneCB()
     glGenBuffers(1, &VBO); 
     glBindBuffer(GL_ARRAY_BUFFER, VBO); 
     glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW); 
-
     glEnableVertexAttribArray(0); 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
     glDrawArrays(GL_TRIANGLES, 0, 3);
     glDisableVertexAttribArray(0);
