40ab0542e60bb5e3b978a0a3d58b7f92c51f0b5e
Эмиль Сафин <safinemil32@gmail.com>
2022-04-15 17:25:10 +0200

    с шейдерами

--- Grafics_1/Source.cpp
+++ Grafics_1/Source.cpp
@@ -1,169 +1,195 @@
-#define _USE_MATH_DEFINES 
-#include "GL/glew.h"
-#include <math.h>
-#include "GL/freeglut.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include "GL/glut.h"
-#include <glm/glm.hpp>
-#include "glm/vec3.hpp"
+#include <string.h>
 #include <assert.h>
+#include <math.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
 #include "pipeline.h"
+
+#define WINDOW_WIDTH 1024
+#define WINDOW_HEIGHT 768
+
 GLuint VBO;
-float Scale = 0.0f;
-const int width = 300;
-const int height = 150;
+GLuint IBO;
+GLuint gWorldLocation;
 
-glm::mat4 Conversion(glm::mat4& m) {
-    m[0][0] = sinf(Scale); m[0][1] = 0.0f;        m[0][2] = 0.0f;        m[0][3] = 0.0f;
-    m[1][0] = 0.0f;        m[1][1] = cosf(Scale); m[1][2] = 0.0f;        m[1][3] = 0.0f;
-    m[2][0] = 0.0f;        m[2][1] = 0.0f;        m[2][2] = sinf(Scale); m[2][3] = 0.0f;
-    m[3][0] = 0.0f;        m[3][1] = 0.0f;        m[3][2] = 0.0f;        m[3][3] = 1.0f;
-    return m;
-}
 
-glm::mat4 Rotation(glm::mat4& m) {
-    m[0][0] = cosf(Scale); m[0][1] = -sinf(Scale); m[0][2] = 0.0f; m[0][3] = 0.0f;
-    m[1][0] = sinf(Scale); m[1][1] = cosf(Scale);  m[1][2] = 0.0f; m[1][3] = 0.0f;
-    m[2][0] = 0.0f;        m[2][1] = 0.0f;         m[2][2] = 1.0f; m[2][3] = 0.0f;
-    m[3][0] = 0.0f;        m[3][1] = 0.0f;         m[3][2] = 0.0f; m[3][3] = 1.0f;
-    return m;
-}
+static const char* pVS = "                                                          \n\
+#version 330                                                                        \n\
+                                                                                    \n\
+layout (location = 0) in vec3 Position;                                             \n\
+                                                                                    \n\
+uniform mat4 gWorld;                                                                \n\
+                                                                                    \n\
+out vec4 Color;                                                                     \n\
+                                                                                    \n\
+void main()                                                                         \n\
+{                                                                                   \n\
+    gl_Position = gWorld * vec4(Position, 1.0);                                     \n\
+    Color = vec4(clamp(Position, 0.0, 1.0), 1.0);                                   \n\
+}";
 
-glm::mat4 Moving(glm::mat4& m) {
-    m[0][0] = 1.0f;  m[0][1] = 0.0f;  m[0][2] = 0.0f; m[0][3] = sinf(Scale);
-    m[1][0] = 0.0f;  m[1][1] = 1.0f;  m[1][2] = 0.0f; m[1][3] = 0.0f;
-    m[2][0] = 0.0f;  m[2][1] = 0.0f;  m[2][2] = 1.0f; m[2][3] = 0.0f;
-    m[3][0] = 0.0f;  m[3][1] = 0.0f;  m[3][2] = 0.0f; m[3][3] = 1.0f;
-    return m;
-}
-
-glm::mat4 InitPerspectiveProj(float fovy, float aspect, float znear, float zfar) {
-    glm::mat4 m;
-    const float zRange = znear - zfar;
-    const float tanHalfFOV = tanf(fovy / 2.0 * M_PI / 180);
-
-    m[0][0] = 1.0f / (tanHalfFOV * aspect);
-    m[0][1] = 0.0f;
-    m[0][2] = 0.0f;
-    m[0][3] = 0.0f;
-
-    m[1][0] = 0.0f;
-    m[1][1] = 1.0f / tanHalfFOV;
-    m[1][2] = 0.0f;
-    m[1][3] = 0.0f;
-
-    m[2][0] = 0.0f;
-    m[2][1] = 0.0f;
-    m[2][2] = (-znear - zfar) / zRange;
-    m[2][3] = 2.0f * zfar * znear / zRange;
-
-    m[3][0] = 0.0f;
-    m[3][1] = 0.0f;
-    m[3][2] = 1.0f;
-    m[3][3] = 0.0f;
-
-    return m;
-}
-
-glm::vec3 Cross(const glm::vec3 v1, const glm::vec3 v2) {
-    const float _x = v1.y * v2.z - v1.z * v2.y;
-    const float _y = v1.z * v2.x - v1.x * v2.z;
-    const float _z = v1.x * v2.y - v1.y * v2.x;
-
-    return glm::vec3(_x, _y, _z);
-}
-
-glm::vec3 Normalize(glm::vec3 v)
-{
-    const float Length = sqrtf(v.x * v.x + v.y * v.y + v.z * v.z);
-
-    v.x /= Length;
-    v.y /= Length;
-    v.z /= Length;
-
-    return glm::vec3(v.x, v.y, v.z);
-}
-
-glm::mat4 InitCameraTransform(const glm::vec3& Target, const glm::vec3& Up)
-{
-    glm::mat4 m;
-
-    glm::vec3 N = Target;
-    N = Normalize(N);
-    glm::vec3 U = Up;
-    U = Normalize(U);
-    U = Cross(U, Target);
-    glm::vec3 V = Cross(N, U);
-
-    m[0][0] = U.x; m[0][1] = U.y; m[0][2] = U.z; m[0][3] = 0.0f;
-    m[1][0] = V.x; m[1][1] = V.y; m[1][2] = V.z; m[1][3] = 0.0f;
-    m[2][0] = N.x; m[2][1] = N.y; m[2][2] = N.z; m[2][3] = 0.0f;
-    m[3][0] = 0.0f; m[3][1] = 0.0f; m[3][2] = 0.0f; m[3][3] = 1.0f;
-
-    return m;
-}
+static const char* pFS = "                                                          \n\
+#version 330                                                                        \n\
+                                                                                    \n\
+in vec4 Color;                                                                      \n\
+                                                                                    \n\
+out vec4 FragColor;                                                                 \n\
+                                                                                    \n\
+void main()                                                                         \n\
+{                                                                                   \n\
+    FragColor = Color;                                                              \n\
+}";
 
 static void RenderSceneCB()
 {
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glm::vec3 Vertices[3];
-    Vertices[0] = glm::vec3(-1.0f, -1.0f, 0.0f);
-    Vertices[1] = glm::vec3(1.0f, -1.0f, 0.0f);
-    Vertices[2] = glm::vec3(0.0f, 1.0f, 0.0f);
+    static float Scale = 0.0f;
 
-    Scale += 0.001f;
+    Scale += 0.1f;
 
-    glm::mat4 mtconv = Conversion(mtconv); //создаем матрицу
-    glm::mat4 mtrotation = Rotation(mtrotation);
-    glm::mat4 mtmove = Moving(mtmove);
-    glm::mat4 proj = InitPerspectiveProj(120.0f, (float)width / (float)height, 1.0f, 2000.0f);
+    Pipeline p;
+    p.Rotate(0.0f, Scale, 0.0f);
+    p.WorldPos(0.0f, 0.0f, 5.0f);
+    p.SetPerspectiveProj(30.0f, WINDOW_WIDTH, WINDOW_HEIGHT, 1.0f, 100.0f);
 
-    glm::vec3 CameraPos = glm::vec3(1.0f, 1.0f, -3.0f);
-    glm::vec3 CameraTarget = glm::vec3(0.45f, 0.0f, 1.0f);
-    glm::vec3 CameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+    glUniformMatrix4fv(gWorldLocation, 1, GL_TRUE, (const GLfloat*)p.GetTrans());
 
-    CameraPos = Cross(CameraPos, CameraUp);
-    CameraUp = Normalize(CameraUp);
-    glm::mat4 mtcam = InitCameraTransform(CameraTarget, CameraUp);
-
-    Vertices[0] = glm::vec4(Vertices[0], 1.0f) * proj * mtcam * mtmove * mtrotation * mtconv;
-    Vertices[1] = glm::vec4(Vertices[1], 1.0f) * proj * mtcam * mtmove * mtrotation * mtconv;
-    Vertices[2] = glm::vec4(Vertices[2], 1.0f) * proj * mtcam * mtmove * mtrotation * mtconv;
-
-    glGenBuffers(1, &VBO); 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO); 
-    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW); 
-    glEnableVertexAttribArray(0); 
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+
+    glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+
     glDisableVertexAttribArray(0);
-    glutSwapBuffers(); 
+
+    glutSwapBuffers();
 }
 
 
-
-
-
-int main(int argc, char* argv[]) {
-
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowSize(1024, 768);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("laba 2");
+static void InitializeGlutCallbacks()
+{
     glutDisplayFunc(RenderSceneCB);
     glutIdleFunc(RenderSceneCB);
+}
 
+static void CreateVertexBuffer()
+{
+    Vector3f Vertices[4];
+    Vertices[0] = Vector3f(-1.0f, -1.0f, 0.5773f);
+    Vertices[1] = Vector3f(0.0f, -1.0f, -1.15475);
+    Vertices[2] = Vector3f(1.0f, -1.0f, 0.5773f);
+    Vertices[3] = Vector3f(0.0f, 1.0f, 0.0f);
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+}
+
+static void CreateIndexBuffer()
+{
+    unsigned int Indices[] = { 0, 3, 1,
+                               1, 3, 2,
+                               2, 3, 0,
+                               0, 2, 1 };
+
+    glGenBuffers(1, &IBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
+}
+
+static void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
+{
+    GLuint ShaderObj = glCreateShader(ShaderType);
+
+    if (ShaderObj == 0) {
+        fprintf(stderr, "Error creating shader type %d\n", ShaderType);
+        exit(0);
+    }
+
+    const GLchar* p[1];
+    p[0] = pShaderText;
+    GLint Lengths[1];
+    Lengths[0] = strlen(pShaderText);
+    glShaderSource(ShaderObj, 1, p, Lengths);
+    glCompileShader(ShaderObj);
+    GLint success;
+    glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        GLchar InfoLog[1024];
+        glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
+        fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
+        exit(1);
+    }
+
+    glAttachShader(ShaderProgram, ShaderObj);
+}
+
+static void CompileShaders()
+{
+    GLuint ShaderProgram = glCreateProgram();
+
+    if (ShaderProgram == 0) {
+        fprintf(stderr, "Error creating shader program\n");
+        exit(1);
+    }
+
+    AddShader(ShaderProgram, pVS, GL_VERTEX_SHADER);
+    AddShader(ShaderProgram, pFS, GL_FRAGMENT_SHADER);
+
+    GLint Success = 0;
+    GLchar ErrorLog[1024] = { 0 };
+
+    glLinkProgram(ShaderProgram);
+    glGetProgramiv(ShaderProgram, GL_LINK_STATUS, &Success);
+    if (Success == 0) {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    glValidateProgram(ShaderProgram);
+    glGetProgramiv(ShaderProgram, GL_VALIDATE_STATUS, &Success);
+    if (!Success) {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    glUseProgram(ShaderProgram);
+
+    gWorldLocation = glGetUniformLocation(ShaderProgram, "gWorld");
+    assert(gWorldLocation != 0xFFFFFFFF);
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Tutorial 12");
+
+    InitializeGlutCallbacks();
+
+    // Must be done after glut is initialized!
     GLenum res = glewInit();
-    if (res != GLEW_OK)
-    {
+    if (res != GLEW_OK) {
         fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
         return 1;
     }
+
+    glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+    CreateVertexBuffer();
+    CreateIndexBuffer();
+
+    CompileShaders();
+
     glutMainLoop();
 
     return 0;
 }
-
